6e20eac9f6437e02f21d281ec690cf52aa268ef5
*andrewxx* <*andrewxxpro@gmail.com*>
2024-09-19 19:28:03 -0400

    added paddle speed

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import pygame
 
-WIDTH,HEIGHT = 1280,720
+WINDOW_WIDTH,WINDOW_HEIGHT = 1280,720
 fps = 60
 
 #creating paddle with rect
 paddle_width = 330
 paddle_height = 35
 paddle_speed = 15
-paddle = pygame.Rect(WIDTH//2 - paddle_width//2,HEIGHT - paddle_height -10,paddle_width,paddle_height)
+paddle = pygame.Rect(WINDOW_WIDTH//2 - paddle_width//2,WINDOW_HEIGHT - paddle_height -10,paddle_width,paddle_height)
 
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
+screen = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 clock = pygame.time.Clock()
+
 
 
 #game loop
@@ -20,6 +21,17 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+
+
+    #controls
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and paddle.left > 0:
+        paddle.left -= paddle_speed
+
+    if key[pygame.K_RIGHT] and paddle.right <WINDOW_WIDTH:
+        paddle.right += paddle_speed
+
+
 
 
     #draw the game
