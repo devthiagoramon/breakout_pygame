e7286150693e40d73e04c393e1e2481f6e8ed674
*andrewxx* <*andrewxxpro@gmail.com*>
2024-09-19 23:28:20 -0400

    added horizontal movement to the ball

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import mechanics as mec
 import constants as consts
 from random import randrange as rand
 
+from lpc2024.atividade004.breakout_pygame.mechanics import ball_dx
+
 #paddle
 paddle = pygame.Rect(consts.WINDOW_WIDTH // 2 - consts.PADDLE_WIDTH // 2,
                      consts.WINDOW_HEIGHT - consts.PADDLE_HEIGHT - 20, consts.PADDLE_WIDTH,
@@ -10,9 +12,11 @@ paddle = pygame.Rect(consts.WINDOW_WIDTH // 2 - consts.PADDLE_WIDTH // 2,
 #ball
 ball_size = 10
 ball = pygame.Rect(rand(ball_size, consts.WINDOW_WIDTH - ball_size),consts.WINDOW_HEIGHT//2,ball_size,ball_size)
-ball_dx = -1
-ball_dy = -1
 
+#walls
+left_wall = pygame.Rect(0,0,10,consts.WINDOW_HEIGHT)
+right_wall  = pygame.Rect(consts.WINDOW_WIDTH-10,0,10,consts.WINDOW_HEIGHT)
+upper_wall = pygame.Rect(0,0,consts.WINDOW_WIDTH,30)
 
 pygame.init()
 screen = pygame.display.set_mode((consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
@@ -24,9 +28,28 @@ while True:
             pygame.quit()
     screen.fill(consts.BLACK)
 
-    # draw
+    # drawing the game
     pygame.draw.rect(screen, consts.BLUE, paddle)
     pygame.draw.rect(screen, consts.WHITE, ball)
+    pygame.draw.rect(screen, consts.WHITE, left_wall)
+    pygame.draw.rect(screen, consts.WHITE, right_wall)
+    pygame.draw.rect(screen, consts.WHITE, upper_wall)
+
+    #ball movement
+    ball.x += consts.BALL_SPEED * mec.ball_dx
+    #ball.y += consts.BALL_SPEED * mec.ball_dy
+
+    #ball collision with walls
+
+    if ball.x > consts.WINDOW_WIDTH - 10:
+        mec.ball_hit_wall()
+
+    if ball.x < 10:
+        mec.ball_hit_wall()
+
+
+    #if ball.y < consts.WINDOW_HEIGHT - consts.PADDLE_HEIGHT:
+        #if ball.x + 10
 
     # controls
 
